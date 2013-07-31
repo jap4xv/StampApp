0df66b0c3acb3ef0ef6951471495347167239b42
Alex Politis <alex@alex-Inspiron-7520.(none)>
2013-07-31 15:54:23 -0400

    prod v4

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,5 @@ class Admin < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :registerable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 end
