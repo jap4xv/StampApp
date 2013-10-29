94177d37811f0f6fc253a9620d21a664eab5c553
Alex <alex@alex-Inspiron-7520.(none)>
2013-10-29 12:09:03 -0400

    search fix

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -5,7 +5,7 @@ class Stamp < ActiveRecord::Base
 	has_many :categorizations
   has_many :categories, through: :categorizations
 	accepts_nested_attributes_for :assets, :allow_destroy => true
-	self.per_page = 30
+	self.per_page = 25
 
   
 	has_attached_file :image, :styles => {  :large => "600x600>", :medium => "300x300>", :thumb => "128x128>" }, :default_url => "/images/:style/missing.png"
@@ -18,19 +18,19 @@ class Stamp < ActiveRecord::Base
     if Rails.env.development?
       if search
         #find(:all, :conditions => ["\"Title\" LIKE ?", "%#{search}%"] )
-        Stamp.where(["\"Title\" LIKE ?" , "%#{search}%"] ).all
+        where(["\"Title\" LIKE ?" , "%#{search}%"] )
       else
-        Stamp.all
+        scoped
       end
     else
       if search
-        Stamp.where("\"Title\" @@ :s or \"Description\" @@ :s" , s: search ).all
+        where("\"Title\" @@ :s or \"Description\" @@ :s" , s: search )
       else
-        Stamp.all
+        scoped
       end
     end
   end
-
+  
 	#validates_attachment_presence :image
 	#validates_attachment_size :image, :less_than => 5.megabytes
 	#validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
